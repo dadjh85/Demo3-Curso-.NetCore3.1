f83a2b1880e6c5840039e2383cba1575c0673fa9
David Jiménez <david.jimenez@altia.es>
2020-07-20 17:51:23 +0200

    * Added Logs

---
+++ src/Demo3/Demo3/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Demo3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public IActionResult GetList()
+        {
+            _logger.LogWarning("test log warning");
+            _logger.LogError("test log error");
+            _logger.LogCritical("test log critical");
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Demo3/Demo3/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Demo3.Controllers
+{
+    /// <summary>
+    /// Controller of CRUD of User
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public IActionResult GetList()
+        {
+            _logger.LogInformation("test log information");
+            return Ok();
+        }
+
+    }
+}
